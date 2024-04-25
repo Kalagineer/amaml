248fefee600e174b3c84f55b2548b5c4e6cf972b
Jose <gearoid.exe@gmail.com>
2024-04-25 17:01:20 +0200

    uploader debuggin flag + outputname

--- uploader.sh
+++ uploader.sh
@@ -1,48 +1,77 @@
 #!/bin/bash
 
+db=true
+
 #############################################################################################
 # Important variables
 #
-apiKey="21d6295d73722b2dd88acf93f58c4bcfcb705d61"
+apiKey="c6427e9ae9d3085bdf4727a1470d99f6761bdc4f"
 url="https://api.malcore.io/api/upload"
-filename="/home/pepe/Desktop/TFG/amaml/danger/a684bc249fdcaaae006ab429d046c28147ae7430.exe"
+filename=$1
+output_filename="status-$1.json"
 
 state="running"
 
 response=$(curl -F "filename1=@$filename" -X POST -H "apiKey: $apiKey" -H "X-No-Poll: true" $url)
+
+if [[ $db = true ]]
+then
+    echo " "
+    echo "$response"
+    echo " "
+fi
+
 uuid=$(echo "$response" | jq -r '.data.data.uuid')
+
+if [[ $db = true ]]
+then
+    echo " "
+    echo "$uuid"
+    echo " "
+fi
+
 status=$(curl -X POST https://api.malcore.io/api/status --data "uuid=$uuid" -H "apiKey: $apiKey")
+
+
+if [[ $db = true ]]
+then
+    echo " "
+    echo "$status"
+    echo " "
+fi
 
 while [[ $state = "running" ]]
 do
     status=$(curl -X POST https://api.malcore.io/api/status --data "uuid=$uuid" -H "apiKey: $apiKey")
+    
+    if [[ $db = true ]]
+    then
+        echo " "
+        echo "$status"
+        echo " "
+    fi
+
     state=$(echo "$status" | jq -r '.data.status')
+
+    if [[ $db = true ]]
+    then
+        echo " "
+        echo "$state"
+        echo " "
+    fi
+
     echo "In progress..."
     sleep 5
 done
 
 echo "The file has been analyzed."
 
-echo "$status" > status_2.json
+echo " "
+echo "$status"
+echo " "
 
-#fileType=$(echo '$status' | jq -r '.data.threat_summary.results.file_type')
-#fileThreatScore=$(echo '$status' | jq -r '.data.threat_score.results.score')
-#fileEntropy=$(echo '$status' | jq -r '.data.threat_score.results.signatures[2]')
-#fileAIClass=$(echo '$status' | jq -r '.data.threat_score.results.signatures[9]')
+echo "$status" > $output_filename
 
-
-
-#echo "RESULT:"
-#echo "The file type is: $fileType"
-#echo "The overall Threat Score is: $fileThreatScore"
-#echo "The level of entropy of the file is: $fileEntropy"
-#echo "The AI Classification done by Malcore is: $fileAIClass"
-
-
-# shell commands for debugging pourposes
-#
-# curl -X POST https://api.malcore.io/api/status --data "uuid=6e18915d06977e20-2fa4f1ad-a8c944db-ef3e1a06-12ab6293-8a95241b6b23f929" -H "apiKey: 21d6295d73722b2dd88acf93f58c4bcfcb705d61"
-# cat status_1.json | jq -r '.data.threat_score.results.signatures' | length
 
 
 
